d241a42b57fdd1abd6ec0ee260a966448fa13b9c
Jenell Pizarro <nellarro@gmail.com>
2016-08-13 00:51:34 -0400

    Been working on this for hours with no luck.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+// import smile from './pics/smile.gif'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-container'>
+          <div className='App-header'>
+          <h1>Oh, Look! A picture!</h1>
+          </div>
+        </div>
+        <p></p>
+      </div>
+    )
+  }
+}
+
+export default App
