9727d85af4df21486094242c0e435ec9d78fc3c2
Jenell Pizarro <nellarro@gmail.com>
2016-08-13 13:07:21 -0400

    just looking at other things for the moment.

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,15 @@ import './App.css'
 class Home extends Component {
   render () {
     return (
-      <div><h1>Home-sies!</h1></div>
+      <div>
+        <div className='Home-header'>
+          <h1>Home-sies!</h1>
+        </div>
+        <div className='Home-container'>
+          <div className='Home-image'>
+          </div>
+        </div>
+      </div>
     )
   }
 }
